a33dde9fa024fbfae728f31b05bc201a7226be69
prabhakar <prabhakarkumar16181@gmail.com>
2024-03-08 00:43:00 -0800

    className link to navBar-item changing

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -3,9 +3,9 @@ import { NavLink } from "react-router-dom";
 function NavBar(){
     return (
         <div className="navBar">
-            <NavLink className="link" to="/">Home</NavLink>
-            <NavLink className="link" to="/about">About Us</NavLink>
-            <NavLink className="link" to="/blog">Blog</NavLink>
+            <NavLink className="navBar-item" to="/">Home</NavLink>
+            <NavLink className="navBar-item" to="/about">About Us</NavLink>
+            <NavLink className="navBar-item" to="/blog">Blog</NavLink>
         </div>
     )
 }
