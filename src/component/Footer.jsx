f3d45912597e872f138fffd26c5e61c739635a21
prabhakar <prabhakarkumar16181@gmail.com>
2024-03-09 02:48:16 -0800

    useNavigate used in Footer

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,12 +1,29 @@
+import {useNavigate} from 'react-router-dom'
+
 function Footer() {
+  const navigate = useNavigate();
+
+  function navToPage(pageName){
+    switch(pageName){
+      case "Blog":
+        navigate('/blog')
+        break;
+      case "About":
+        navigate('/about')
+        break;
+      default:
+        navigate('/')
+    }
+  }
+
   return (
     <div className="footer">
       <p>&copy; copyright reserved @prabhakar</p>
       <div>
         <h3>Pages</h3>
-        <button>Home</button>
-        <button>About</button>
-        <button>Blog</button>
+        <span onClick={()=> navToPage('Home')}>Home</span>
+        <button onClick={()=> navToPage('About')}>About</button>
+        <button onClick={()=> navigate('Blog')}>Blog</button>
       </div>
     </div>
   )
