46e7773347e9c7d1c2c8ef9cd2ba21dd4f3cb3c8
prabhakar <prabhakarkumar16181@gmail.com>
2024-03-08 23:40:31 -0800

    Footer Styled

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -2,6 +2,12 @@ function Footer() {
   return (
     <div className="footer">
       <p>&copy; copyright reserved @prabhakar</p>
+      <div>
+        <h3>Pages</h3>
+        <button>Home</button>
+        <button>About</button>
+        <button>Blog</button>
+      </div>
     </div>
   )
 }
